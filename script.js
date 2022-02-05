a0dc3b7d8cc3bedc8f81ba4276812affab59edc7
Justin Liu <exovitejl@gmail.com>
2022-02-05 17:16:52 -0500

    Add alternating text

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 //
 /*
 */
+
+// Alternating text of my several title descriptions
+{
+    var interval = setInterval(changeText, 1000);
+    var counter = 0;
+    var titles = ["programmer", "NAV fan", "photographer (kinda)", "poet (kinda)", "singer (kinda)", "high elo League of Legends player", "League of Legends warding master", "vegetarian"]
+}
+
+function initialize() {
+    // grab the innerHTML of the object with the id "flashing-text"
+    altText = document.getElementById("altText");
+}
+
+function changeText() {
+    if(counter === 8) counter = 0;
+    altText.innerHTML = titles[counter];
+    counter++;
+}
